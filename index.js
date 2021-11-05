09a83a207692ff5910e74c9488b6d8db82763534
Denver-Meadows <meadows8200@gmail.com>
2021-11-05 13:02:24 -0400

    Setup server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// git remote add origin https://github.com/Denver-Meadows/inventory_plus_app.git
+import express from "express";
+const app = express();
+
+// app.use runs everytime we get an incoming response
+app.use((req, res) => {
+  res.send("Get that server working");
+});
+
+app.listen(3001, () => {
+  console.log("Serving on port 3001");
+});
