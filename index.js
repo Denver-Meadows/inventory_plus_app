f2608027af1b9c4ffb83631ecf7eda04c381bc75
Denver-Meadows <meadows8200@gmail.com>
2021-11-06 19:40:30 -0400

    working with the server

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ import express from "express";
 const app = express();
 
 // app.use runs everytime we get an incoming response
-app.use((req, res) => {
-  res.send("Get that server working");
+// app.use((req, res) => {
+//   res.send("Get that server working");
+// });
+
+app.get("/", (req, res) => {
+  res.send("home page");
 });
+
+app.get("/cats", (req, res) => {
+  res.send("MEOW");
+});
+
+app.get("*", (req, res) => {
+  res.send("i don't know that path");
+});
+
+//
+
+//
 
 app.listen(3001, () => {
   console.log("Serving on port 3001");
