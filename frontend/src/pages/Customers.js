8b0f7e4a8b295b7f33e6e7b252a2b2b811385a99
Denver-Meadows <meadows8200@gmail.com>
2021-11-12 17:38:21 -0500

    Frontend receiving data from DB

--- frontend/src/pages/Customers.js
+++ frontend/src/pages/Customers.js
@@ -2,20 +2,26 @@ import React, { useState, useEffect } from "react";
 import DashboardLink from "../components/DashboardLink";
 import TopInfoBar from "../components/TopInfoBar";
 import SideNav from "../components/SideNav";
+import axios from "axios";
+
+////////////////   NEED TO MAKE A COMPONENT WITH THE RETURN CUSTOMER DATA
 
 const Customers = () => {
   const [customers, setCustomers] = useState([]);
 
   // By setting the proxy in package.json to localhost:3001, the /customers routes are now the same (in theory)
+  // Commented out the way to get this data prior to using axios.
   const getCustomers = async () => {
-    const res = await fetch("/customers");
-    const data = await res.json();
+    const { data } = await axios.get("/customers");
+    // const data = await res.json();
     // setCustomers(data.customerList);
+    // console.log(data);
     setCustomers(data);
   };
 
   useEffect(() => {
     getCustomers();
+    console.log(customers);
   }, []);
 
   return (
@@ -25,10 +31,20 @@ const Customers = () => {
         <SideNav />
         <div className="single-page-overview">
           <h1>review customers</h1>
-          {/* {customers.map((customer, index) => {
-            const { name, age } = customer;
-            return <p key={index}>{`Name:  ${name} Age: ${age}`}</p>;
-          })} */}
+          {customers.map((customer) => {
+            const { name, email, phone, city, state, _id } = customer;
+            return (
+              <p key={_id}>
+                {`
+              Name:  ${name} 
+              Email: ${email}
+              Phone: ${phone}
+              City: ${city}
+              State: ${state}
+              `}
+              </p>
+            );
+          })}
         </div>
       </div>
     </main>
