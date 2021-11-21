8fdcca9270a6f337213b216de76935a44c8eca94
Denver-Meadows <meadows8200@gmail.com>
2021-11-20 21:04:28 -0500

    numOfUsers complete

--- frontend/src/components/NumOfUsers.js
+++ frontend/src/components/NumOfUsers.js
@@ -1,6 +1,14 @@
 import { FcConferenceCall, FcOrganization } from "react-icons/fc";
+import { useGlobalContext } from "./context";
 
 function NumOfUsers() {
+  const { customers, suppliers } = useGlobalContext();
+
+  const numOfPeople = (items) => {
+    const num = items.flat(1);
+    return num.length;
+  };
+
   return (
     <article className="dashboard-content-num-users inventory-overview-article">
       <h3>Number of Users</h3>
@@ -8,12 +16,12 @@ function NumOfUsers() {
         <div className="inventory-overview-article-content-summary">
           <FcConferenceCall className="inventory-icon" />
           <p>Total Customers</p>
-          <h3>112</h3>
+          <h3>{numOfPeople(customers)}</h3>
         </div>
         <div className="inventory-overview-article-content-summary">
           <FcOrganization className="inventory-icon" />
           <p>Total Suppliers</p>
-          <h3>12</h3>
+          <h3>{numOfPeople(suppliers)}</h3>
         </div>
       </div>
     </article>
