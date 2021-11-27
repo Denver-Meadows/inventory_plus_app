7f836f7661b30a8f9926e3bddf20a6b24577502a
Denver-Meadows <meadows8200@gmail.com>
2021-11-27 16:21:29 -0500

    code cleanup

--- frontend/src/pages/CreateSupplier.js
+++ frontend/src/pages/CreateSupplier.js
@@ -1,7 +1,6 @@
 import React from "react";
 import TopInfoBar from "../components/TopInfoBar";
 import SideNav from "../components/SideNav";
-import { Link } from "react-router-dom";
 import CreateSupplierForm from "../components/CreateSupplierForm";
 
 const CreateSupplier = () => {
