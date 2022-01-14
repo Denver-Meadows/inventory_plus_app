26537373bf842a3709446d0e1fa012327ca86f53
Denver-Meadows <meadows8200@gmail.com>
2022-01-13 20:27:34 -0500

    bug fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const supplierRoute = require("./routes/supplierRoute");
 
 // connect to mongo with mongoose
 const dbURL =
-  process.env > DB_URL || "mongodb://localhost:27017/inventory_plus_app";
+  process.env.DB_URL || "mongodb://localhost:27017/inventory_plus_app";
 mongoose
   .connect(dbURL, {
     useNewUrlParser: true,
