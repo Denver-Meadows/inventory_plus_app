4aec7f97eb5f34bf38c4447d75e37a8f0bdefdab
Denver-Meadows <meadows8200@gmail.com>
2022-01-13 19:51:11 -0500

    updating index and json for deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // Routes
 const customerRoute = require("./routes/customerRoute");
